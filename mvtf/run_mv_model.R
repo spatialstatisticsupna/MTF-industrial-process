149221691227c15282d3f845bb78b2b2419cde80
Erlantz Miguélez García <67976555+ErlantzMGMG@users.noreply.github.com>
2024-07-10 17:52:28 +0200

    Update run_mv_model.R
    Extension to include VARX models

--- mvtf/run_mv_model.R
+++ mvtf/run_mv_model.R
@@ -4,6 +4,7 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 # Libraries 
 
 library(tidyverse)
+library(MTS)        # for VARX models
 
 source('functions.R')
 source('update_model.R')
@@ -59,9 +60,9 @@ OEEbw = readRDS('../data/oee_data_4weeks.Rds')
 # Variable selection #
 ######################
 
-rates = c('lo','av','pf','qu','oee')
+rates = c('av','pf','oee')
 
-yn = c('OpT','NOpT','VT')                       # response variables
+yn = c('OpT','NOpT')                            # response variables
 tn = c(rates, 'OT', 'rcs', 'TU')                # classification variables
 xn = c('new.of','new.sh','ics')                 # co-variates
 ini.st.var = 'new.sh'
@@ -82,7 +83,10 @@ weeks = OEEbw$week.id %>% unique()
 LAGS  = 0:5
 
 
-ForecastResults = ForecastList(yn, nrow(OEEbw), c(paste0('q',LAGS),'persistence'))
+########################## 
+# Object to save results #
+##########################
+ForecastResults = ForecastList(yn, nrow(OEEbw), c(paste0('q',LAGS),'persistence',paste0('VARX_',LAGS)))
 for (mod in names(ForecastResults)) {
   for (v in yn) {
     ForecastResults[[mod]][[v]]$shift = interaction(OEEbw$wday,OEEbw$tday,sep='')
@@ -94,7 +98,7 @@ for (mod in names(ForecastResults)) {
 
 Fi = 0
 
-for (wk in 1:length(weeks)) {   # start loop for weeks
+for (wk in 1:length(weeks)) {
   OEEtr = OEEbw %>% filter(week.id %in% weeks[-wk])
   OEEte = OEEbw %>% filter(week.id == weeks[wk])
   N0    = nrow(OEEtr)
@@ -141,6 +145,16 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
                           OEEtr[,union(xn,ini.st.var)]
   )
   
+  # Pull out data for VARX model
+  ytr = as.matrix(OEEtr[,yn],rownames.force=T)
+  xtr = as.matrix(OEEtr[,xn],rownames.force=T)
+  xtr = cbind(xtr,dummy.code(OEEtr$wday))
+  xtr = cbind(xtr,dummy.code(OEEtr$tday))
+  # Set Monday Morning as baseline shift
+  bs  = c('Mo','M')
+  xtr = xtr[,setdiff(colnames(xtr),bs)]
+  
+  
   # Min-max normalization for further use in the knn assignment
   maxTrain = apply(ClassTrain[,tn],2,max) %>% as.numeric()                 
   minTrain = apply(ClassTrain[,tn],2,min) %>% as.numeric()  
@@ -152,7 +166,7 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
   #################
   # Learning Step #
   #################
-  for (ii in 1:length(LAGS)) {   # start loop for lags in the learning step
+  for (ii in 1:length(LAGS)) {
     q = LAGS[ii]
     qname = paste0('q',q)
     Parameters[[qname]] = Initialization(
@@ -174,10 +188,24 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
   # (method for cluster assignment: knn / mahalanobis)
   cluster.assign = 'knn'
   
-  for (ii in 1:length(LAGS)) {   # start loop for lags in the forecasting step
+  for (ii in 1:length(LAGS)) {
     q = LAGS[ii]
-    qname = paste0('q',q)
+    qname  = paste0('q',q)
+    vxname = paste0('VARX_',q)
     Params = Parameters[[qname]]
+    
+    # VARX model:    > STATIC!!, meaning that coefficients do not update (Eqs. [3.3a - 3.3d]) with new information
+    #                > Do not include the classes in the model (hidden states in Fig. [3.1], or Eq. [3.2b] in the paper)
+    varx = MTS::VARX(zt=ytr,p=q,xt=xtr,m=0,output=FALSE)
+    varx = MTS::refVARX(varx)
+    
+    # Warning!! We have to make the next trick to obtain the predictions in the VARX model with 0 lags 
+    #           (that is, a regular regression)
+    if (q==0) {
+      varx$aror = 1
+      varx$Phi=diag(c(0,0))
+    }
+    
     
     first = ifelse(q==0,N0,N0-q+1)
     ClassTest = rbind(ClassTrain[first:N0,],
@@ -189,21 +217,38 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
                                  OEEte[,union(xn,ini.st.var)]),
                       make.row.names = FALSE)
     
+    # Pull out data for VARX prediction
+    yte = as.matrix(OEEte[,yn],rownames.force=T)
+    xte = as.matrix(OEEte[,xn],rownames.force=T)
+    xte = cbind(xte,dummy.code(OEEte$wday))
+    xte = cbind(xte,dummy.code(OEEte$tday))
+    xte = xte[,colnames(xtr)]
     
     # Prediction
     Centroids = Centers
-    cat('\n  *** Prediction step in model with',q,'lags ... ***')
-    for (i in 1:L) {    # start loop for prediction horizon
+    cat('  *** Prediction step in model with',q,'lags ... ***\n')
+    for (i in 1:L) {
       j = i + Fi
       
+      # MVTF-Model prediction 
       U = predictors(ClassTest, i, yn, q, xn, ini.st.var, 'class')
       forecast = Prediction(Params, U, 'class', c('wday','tday'))
       
-      # compute absolute and root squared errors
+      # VARX model prediction
+      newxt = xte[i,] %>% as.matrix() %>% t()
+      varx.pred = MTS::VARXpred(m1=varx,newxt=newxt)
+
+      # Add last data for the next prediction
+      varx$data = rbind(varx$data,yte[i,])
+      varx$xt   = rbind(varx$xt,xte[i,])
+      
+      
       w    = U[,'wday']
       s    = U[,'tday']
+      prev = U[,'class']
       
       
+      # compute absolute and squared errors
       for (jj in 1:m1) {
         v    = yn[jj]
         
@@ -215,15 +260,30 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
           
         }
         
+        # MVTF forecast
         yhat = forecast$prediction[jj]
         shat = sqrt(forecast$error[jj,jj])
         y    = ForecastResults[[qname]][[v]][j, 'value']
+        lower = yhat-1.96*shat; upper = yhat+1.96*shat
         ForecastResults[[qname]][[v]][j, 'pred']       = yhat
         ForecastResults[[qname]][[v]][j, 'pr.err']     = shat
-        ForecastResults[[qname]][[v]][j, 'lower']      = yhat-1.96*shat
-        ForecastResults[[qname]][[v]][j, 'upper']      = yhat+1.96*shat
+        ForecastResults[[qname]][[v]][j, 'lower']      = lower
+        ForecastResults[[qname]][[v]][j, 'upper']      = upper
         ForecastResults[[qname]][[v]][j, 'abs.err']    = abs(yhat - y)
         ForecastResults[[qname]][[v]][j, 'sq.err']     = (yhat - y)**2
+        ForecastResults[[qname]][[v]][j, 'coverage']   = as.integer(y>=lower & y<=upper)
+        
+        # VARX forecast
+        yhat = varx.pred$pred[jj]
+        shat = varx.pred$se[jj]
+        lower = yhat-1.96*shat; upper = yhat+1.96*shat
+        ForecastResults[[vxname]][[v]][j, 'pred']       = yhat
+        ForecastResults[[vxname]][[v]][j, 'pr.err']     = shat
+        ForecastResults[[vxname]][[v]][j, 'lower']      = lower
+        ForecastResults[[vxname]][[v]][j, 'upper']      = upper
+        ForecastResults[[vxname]][[v]][j, 'abs.err']    = abs(yhat - y)
+        ForecastResults[[vxname]][[v]][j, 'sq.err']     = (yhat - y)**2
+        ForecastResults[[vxname]][[v]][j, 'coverage']   = as.integer(y>=lower & y<=upper)
       }
       
       # update centroids 
@@ -244,7 +304,10 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
       ClassTest[ind,'class'] = nearest_class
       z.new = OEEte[i,union(tn, yn)] 
       nv = ncol(Centroids)-5
-      Centroids = update_centroids(centr.mat=Centroids,new_data=z.new,nc=nearest_class %>% as.integer(),vars=1:nv+1)
+      Centroids = update_centroids(centr.mat=Centroids,
+                                   new_data=z.new,
+                                   nc=nearest_class %>% as.integer(),
+                                   vars=1:nv+1)
       
       # update parameters
       first = ifelse(q==0,ind-1,ind-q)
@@ -253,13 +316,15 @@ for (wk in 1:length(weeks)) {   # start loop for weeks
                            class.var='class', group.vars=c('wday','tday'),
                            lags=q, reini=reini)
     }
-    cat('\n  *** End of prediction step in model with',q,'lags ... ***\n')
+    cat('  *** End of prediction step in model with',q,'lags ... ***\n')
   }
   Fi = Fi + L
 }
 
 
-# gather prediction results in a supermatrix
+
+# Gather prediction results in a supermatrix
+
 models = names(ForecastResults)
 mtf_mv_forecast.aux = vector('list', length=length(models))
 names(mtf_mv_forecast.aux) = models
@@ -273,10 +338,13 @@ rm(mtf_mv_forecast.aux)
 
 
 # Folder to save results
+
 if(!file.exists("../results")) dir.create("../results")
 
+
+
 # Save the results
+
 saveRDS(mtf_mv_forecast, file="../results/MTF_mv_forecast.Rds")
-#save(mtf_mv_forecast, file="results/MTF_mv_forecast.RData")
 
 
